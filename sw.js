a76073c91af0b03e0730a05c566c7550da700800
Marc González Pastor <gonzalezpastor.marc@gmail.com>
2026-09-09 09:42:22 +0200

    Segell del service worker recalculat sobre el contingut de veres
    Fins ara barrejava fitxers amb finals Unix i de Windows segons quins s'havien
    tocat des de l'últim checkout, o siga que no descrivia el que es publica.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: fa que la web tiri sense connexió.
    Puja VERSION quan canviïn els fitxers i els navegadors se'ls tornaran a baixar. */
-const VERSION = 'h6b279fed59';
+const VERSION = 'h3f732246bf';
 const CACHE   = 'kanoodle-' + VERSION;
 
 const ASSETS = [
